722695b99ac90c69afe1abeb88582419bfe4381a
Christian Dupuis <cd@atomist.com>
2018-10-06 22:58:07 +0200

    Update Cd48Application.java

--- src/main/java/com/atomist/Cd48Application.java
+++ src/main/java/com/atomist/Cd48Application.java
@@ -41,6 +41,8 @@ package com.atomist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class Cd48Application {
 
